05046b8e95b6682788f2db2bc90ceded7b78e7a0
astrapi69 <asterios.raptis@gmx.net>
2023-11-04 18:17:12 +0100

    scripts for the list comprehensions lesson

---
+++ list-comprehensions/script-2.py
@@ -0,0 +1,5 @@
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = [len(word) for word in words if word != "the"]
+print(words)
+print(word_lengths)
---
+++ list-comprehensions/script-solution.py
@@ -0,0 +1,8 @@
+from future.types.newlist import newlist
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = []
+for number in numbers:
+    if 0 < number:
+        newlist.append(int(number))
+    
+print(newlist)
