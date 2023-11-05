51c76d1509350d9e78e038137cabb265223ea697
astrapi69 <asterios.raptis@gmx.net>
2023-11-05 16:53:29 +0100

    scripts for the partial functions lesson

---
+++ partial-functions/script-exercise.py
@@ -0,0 +1,5 @@
+#Following is the exercise, function provided:
+from functools import partial
+def func(u, v, w, x):
+    return u*4 + v*3 + w*2 + x
+#Enter your code here to create and print with your partial function
---
+++ partial-functions/script-solution.py
@@ -0,0 +1,7 @@
+#Following is the exercise, function provided:
+from functools import partial
+def func(u, v, w, x):
+    return u*4 + v*3 + w*2 + x
+#Enter your code here to create and print with your partial function
+pFunc = partial(func, 10, 2, 1)
+print(pFunc(12))
