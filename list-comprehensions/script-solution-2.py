44cae86f0892137fe1a2938c50e22248e078910d
astrapi69 <asterios.raptis@gmx.net>
2023-11-13 14:45:35 +0100

    scripts for the 'list comprehensions' lesson

--- list-comprehensions/script-solution-2.py
+++ list-comprehensions/script-solution-2.py
@@ -2,5 +2,6 @@
 # which contains only the positive numbers from the list, as integers.
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
-newlist = []
+newlist = [int(number) for number in numbers if 0 < number]
+    
 print(newlist)
--- list-comprehensions/script-solution.py
+++ list-comprehensions/script-solution.py
@@ -1,4 +1,6 @@
-from future.types.newlist import newlist
+# Using a list comprehension, create a new list called "newlist" out of the list "numbers", 
+# which contains only the positive numbers from the list, as integers.
+
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 newlist = []
 for number in numbers:
