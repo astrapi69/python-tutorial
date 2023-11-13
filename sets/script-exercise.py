05c7f19756053df31e57c18c0df29ca9ebcdc4d2
astrapi69 <asterios.raptis@gmx.net>
2023-11-13 15:20:28 +0100

    extend scripts for the 'sets' lesson

--- sets/script-exercise.py
+++ sets/script-exercise.py
@@ -1,2 +1,3 @@
+# In the exercise below, use the given lists to print out a set containing all the participants from event A which did not attend event B
 a = ["Jake", "John", "Eric"]
 b = ["John", "Jill"]
