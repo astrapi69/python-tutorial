ba75314e4ed9b0810c45e495224912942f4820f6
astrapi69 <asterios.raptis@gmx.net>
2023-11-13 14:49:14 +0100

    extend scripts for the 'lambda functions' lesson

--- lambda-functions/script-exercise.py
+++ lambda-functions/script-exercise.py
@@ -1,3 +1,6 @@
+# Write a program using lambda functions to check if a number 
+# in the given list is odd. Print "True" if the number is odd or "False" if not for each element.
+
 l = [2,4,7,3,14,19]
 for i in l:
     # your code here
--- lambda-functions/script-solution-2.py
+++ lambda-functions/script-solution-2.py
@@ -4,5 +4,5 @@
 l = [2,4,7,3,14,19]
 for i in l:
     # your code here
-    my_lambda = lambda x : (x % 2) != 0
+    my_lambda = lambda x : (x % 2) == 1
     print(my_lambda(i))
