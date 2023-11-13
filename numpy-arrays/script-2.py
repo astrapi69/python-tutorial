29807419274c8d9fd3d402474246c7f4c8236d7f
astrapi69 <asterios.raptis@gmx.net>
2023-11-13 14:20:18 +0100

    scripts for the 'numpy arrays' lesson

---
+++ numpy-arrays/script-2.py
@@ -0,0 +1,12 @@
+# Create 2 new lists height and weight
+height = [1.87,  1.87, 1.82, 1.91, 1.90, 1.85]
+weight = [81.65, 97.52, 95.25, 92.98, 86.18, 88.45]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create 2 numpy arrays from height and weight
+np_height = np.array(height)
+np_weight = np.array(weight)
+
+print(type(np_height))
