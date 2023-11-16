c4964633a1e9852cab22ab9fab70c511b23c44fa
astrapi69 <asterios.raptis@gmx.net>
2023-11-16 16:39:20 +0100

    extend scripts for the 'map, filter and reduce' lesson

--- map-filter-reduce/script-solution.py
+++ map-filter-reduce/script-solution.py
@@ -1,13 +1,21 @@
-#### Map
 from functools import reduce 
 
+# Use map to print the square of each numbers rounded
+# to three decimal places
 my_floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]
+
+# Use filter to print only the names that are less than 
+# or equal to seven letters
 my_names = ["olumide", "akinremi", "josiah", "temidayo", "omoseun"]
+
+filter_result = list(filter(lambda name: 7 <= len(name), my_names))
+# Use reduce to print the product of these numbers
 my_numbers = [4, 6, 9, 23, 5]
 
+# Fix all three respectively.
 map_result = list(map(lambda x: round(x ** 2, 3), my_floats))
-filter_result = list(filter(lambda name: len(name) <= 7, my_names))
-reduce_result = reduce(lambda num1, num2: num1 * num2, my_numbers)
+filter_result = list(filter(lambda name: 7 <= len(name), my_names))
+reduce_result = reduce(lambda num1, num2: num2 if num1 == 0 else num1 * num2, my_numbers, 0)
 
 print(map_result)
 print(filter_result)
